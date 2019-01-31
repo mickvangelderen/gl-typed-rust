4a7d785d6be1c6fd731d534869f9bc50d27daaf2
Mick van Gelderen <mickvangelderen@gmail.com>
2019-01-31 18:18:30 +0100

    Implement basic shader

--- src/lib.rs
+++ src/lib.rs
@@ -1,119 +1,58 @@
 pub mod gl;
 
-use std::num::NonZeroU32;
+// Private.
+#[macro_use]
+mod name;
+
+// Public.
+pub mod shader;
+pub use shader::*;
 
 pub struct GlTyped {
     gl: gl::Gl,
 }
 
-pub trait ShaderKind: Copy {
-    type Name: ShaderName;
-
-    fn as_u32(&self) -> u32;
-}
-
-pub trait ShaderName: Sized {
-    type Kind: ShaderKind;
-
-    fn from_raw_parts(kind: Self::Kind, name: u32) -> Option<Self>;
-
-    fn kind(&self) -> Self::Kind;
-
-    fn as_u32(&self) -> u32;
-}
-
-macro_rules! impl_shader_kinds_and_names {
-    ($($Kind: ident, $Name: ident, $const: ident, $value: expr),*) => {
-        $(
-            #[derive(Debug, Copy, Clone)]
-            #[repr(transparent)]
-            pub struct $Kind(u32);
-
-            impl ShaderKind for $Kind {
-                type Name = $Name;
-
-                #[inline]
-                fn as_u32(&self) -> u32 {
-                    self.0
-                }
-            }
-
-            pub const $const: $Kind = $Kind($value);
-
-            #[derive(Debug)]
-            #[repr(transparent)]
-            pub struct $Name(NonZeroU32);
-
-            impl ShaderName for $Name {
-                type Kind = $Kind;
-
-                #[inline]
-                fn from_raw_parts(_: $Kind, name: u32) -> Option<Self> {
-                    NonZeroU32::new(name).map($Name)
-                }
-
-                #[inline]
-                fn kind(&self) -> Self::Kind {
-                    $const
-                }
-
-                #[inline]
-                fn as_u32(&self) -> u32 {
-                    self.0.get()
-                }
-            }
-        )*
-    }
-}
-
-impl_shader_kinds_and_names!(
-    VertexShaderKind, VertexShaderName, VERTEX_SHADER, gl::VERTEX_SHADER,
-    FragmentShaderKind, FragmentShaderName, FRAGMENT_SHADER, gl::FRAGMENT_SHADER
-);
-
-#[derive(Debug, Copy, Clone)]
-#[repr(u32)]
-pub enum DynamicShaderKind {
-    Vertex = gl::VERTEX_SHADER,
-    Fragment = gl::FRAGMENT_SHADER,
-}
-
-impl ShaderKind for DynamicShaderKind {
-    type Name = DynamicShaderName;
-
-    #[inline]
-    fn as_u32(&self) -> u32 {
-        *self as u32
-    }
-}
-
-pub struct DynamicShaderName {
-    kind: DynamicShaderKind,
-    name: NonZeroU32,
-}
-
-impl ShaderName for DynamicShaderName {
-    type Kind = DynamicShaderKind;
-
-    #[inline]
-    fn from_raw_parts(kind: DynamicShaderKind, name: u32) -> Option<Self> {
-        NonZeroU32::new(name).map(|name| DynamicShaderName { kind, name })
-    }
-
-    #[inline]
-    fn kind(&self) -> Self::Kind {
-        self.kind
-    }
-
-    #[inline]
-    fn as_u32(&self) -> u32 {
-        self.name.get()
-    }
-}
-
 impl GlTyped {
     #[inline]
-    pub unsafe fn create_shader<SN: ShaderName>(&self, kind: SN::Kind) -> Option<SN> {
-        SN::from_raw_parts(kind, self.gl.CreateShader(kind.as_u32()))
+    unsafe fn create_shader_any(&self, kind: ShaderKind) -> Option<ShaderName> {
+        ShaderName::from_u32(self.gl.CreateShader(kind as u32))
+    }
+
+    #[inline]
+    pub unsafe fn create_shader<S: Shader>(&self, kind: S::Kind) -> Option<S> {
+        self.create_shader_any(kind.into())
+            .map(|name| S::from_parts(kind, name))
+    }
+
+    #[inline]
+    unsafe fn delete_shader_any(&self, name: ShaderName) {
+        self.gl.DeleteShader(name.as_u32())
+    }
+
+    #[inline]
+    pub unsafe fn delete_shader<S: Shader>(&self, shader: S) {
+        let (_kind, name) = shader.into_parts();
+        self.delete_shader_any(name);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_generic_shader() {
+        unsafe {
+            let gl: GlTyped = ::std::mem::zeroed();
+            let _shader: ShaderKindName = gl.create_shader(ShaderKind::Vertex).unwrap();
+        }
+    }
+
+    #[test]
+    fn create_specialized_shader() {
+        unsafe {
+            let gl: GlTyped = ::std::mem::zeroed();
+            let _shader: VertexShaderName = gl.create_shader(VERTEX_SHADER).unwrap();
+        }
     }
 }
--- src/shader.rs
+++ src/shader.rs
@@ -1,77 +1,156 @@
-use super::shader_kind::*;
+use crate::gl;
 
+pub trait Shader {
+    type Kind: Into<ShaderKind> + Copy;
+
+    fn from_parts(kind: Self::Kind, name: ShaderName) -> Self;
+
+    fn into_parts(self) -> (Self::Kind, ShaderName);
+
+    fn kind(&self) -> Self::Kind;
+
+    fn name(&self) -> &ShaderName;
+}
+
+/// Kind of a shader.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u32)]
+pub enum ShaderKind {
+    Compute = gl::COMPUTE_SHADER,
+    Vertex = gl::VERTEX_SHADER,
+    TessControl = gl::TESS_CONTROL_SHADER,
+    TessEvaluation = gl::TESS_EVALUATION_SHADER,
+    Geometry = gl::GEOMETRY_SHADER,
+    Fragment = gl::FRAGMENT_SHADER,
+}
+
+/// Name of a shader, without the kind.
 impl_name!(ShaderName);
 
-pub trait StaticShaderName: Sized {
-    type ShaderKind: StaticShaderKind;
-
-    fn as_u32(&self) -> u32;
-    unsafe fn from_raw(name: u32) -> Option<Self>;
+/// Shader kind and name.
+#[derive(Debug)]
+pub struct ShaderKindName {
+    kind: ShaderKind,
+    name: ShaderName,
 }
 
-macro_rules! impl_shader_kind {
-    ($K:path, $T:ident,) => {
-        #[derive(Debug)]
-        #[repr(transparent)]
-        pub struct $T(ShaderName);
+impl Shader for ShaderKindName {
+    type Kind = ShaderKind;
 
-        impl $T {
-            #[inline]
-            pub fn as_u32(&self) -> u32 {
-                self.0.as_u32()
-            }
+    fn from_parts(kind: Self::Kind, name: ShaderName) -> Self {
+        ShaderKindName { kind, name }
+    }
 
-            #[inline]
-            unsafe fn from_raw(name: u32) -> Option<Self> {
-                ShaderName::from_raw(name).map($T)
-            }
-        }
+    fn into_parts(self) -> (Self::Kind, ShaderName) {
+        let ShaderKindName { kind, name } = self;
+        (kind, name)
+    }
 
-        impl StaticShaderName for $T {
-            type ShaderKind = $K;
+    fn kind(&self) -> Self::Kind {
+        self.kind
+    }
 
-            #[inline]
-            fn as_u32(&self) -> u32 {
-                self.as_u32()
-            }
-
-            #[inline]
-            unsafe fn from_raw(name: u32) -> Option<Self> {
-                Self::from_raw(name)
-            }
-        }
-
-        // Temporarily discard the kind.
-        impl AsRef<ShaderName> for $T {
-            #[inline]
-            fn as_ref(&self) -> &ShaderName {
-                &self.0
-            }
-        }
-    };
+    fn name(&self) -> &ShaderName {
+        &self.name
+    }
 }
 
-impl_shader_kind!(
-    ComputeShader,
-    ComputeShaderName,
-);
-impl_shader_kind!(
-    FragmentShader,
-    FragmentShaderName,
-);
-impl_shader_kind!(
-    GeometryShader,
-    GeometryShaderName,
-);
-impl_shader_kind!(
-    VertexShader,
-    VertexShaderName,
-);
-impl_shader_kind!(
-    TessControlShader,
-    TessControlShaderName,
-);
-impl_shader_kind!(
-    TessEvaluationShader,
-    TessEvaluationShaderName,
+macro_rules! impl_shader_kinds_and_names {
+    ($(($Kind: ident, $Name: ident, $const: ident, $value: expr $(,)?)),* $(,)?) => {
+        $(
+            /// Shader kind of which we know the variant at compile-time.
+            #[derive(Debug, Copy, Clone, Eq, PartialEq)]
+            #[repr(transparent)]
+            pub struct $Kind(ShaderKind);
+
+            /// Convert from compile-time variant into run-time variant.
+            impl From<$Kind> for ShaderKind {
+                fn from(kind: $Kind) -> Self {
+                    kind.0
+                }
+            }
+
+            pub const $const: $Kind = $Kind($value);
+
+            /// Shader name of which we know the kind at compile-time.
+            #[derive(Debug)]
+            #[repr(transparent)]
+            pub struct $Name(ShaderName);
+
+            // NOTE(mickvangelderen): I do not want to encourage losing the
+            // kind. Maybe there is a use case for it though.
+            // /// Permanently lose the shader kind.
+            // impl From<$Name> for ShaderName {
+            //     fn from(name: $Name) -> Self {
+            //         name.0
+            //     }
+            // }
+
+            /// Temporarily lose the shader kind.
+            impl AsRef<ShaderName> for $Name {
+                fn as_ref(&self) -> &ShaderName {
+                    &self.0
+                }
+            }
+
+            impl Shader for $Name {
+                type Kind = $Kind;
+
+                fn from_parts(_: Self::Kind, name: ShaderName) -> Self {
+                    $Name(name)
+                }
+
+                fn into_parts(self) -> (Self::Kind, ShaderName) {
+                    ($const, self.0)
+                }
+
+                fn kind(&self) -> Self::Kind {
+                    $const
+                }
+
+                fn name(&self) -> &ShaderName {
+                    self.as_ref()
+                }
+            }
+        )*
+    }
+}
+
+impl_shader_kinds_and_names!(
+    (
+        ComputeShaderKind,
+        ComputeShaderName,
+        COMPUTE_SHADER,
+        ShaderKind::Compute,
+    ),
+    (
+        VertexShaderKind,
+        VertexShaderName,
+        VERTEX_SHADER,
+        ShaderKind::Vertex
+    ),
+    (
+        TessControlShaderKind,
+        TessControlShaderName,
+        TESS_CONTROL_SHADER,
+        ShaderKind::TessControl
+    ),
+    (
+        TessEvaluationShaderKind,
+        TessEvaluationShaderName,
+        TESS_EVALUATION_SHADER,
+        ShaderKind::TessEvaluation
+    ),
+    (
+        GeometryShaderKind,
+        GeometryShaderName,
+        GEOMETRY_SHADER,
+        ShaderKind::Geometry
+    ),
+    (
+        FragmentShaderKind,
+        FragmentShaderName,
+        FRAGMENT_SHADER,
+        ShaderKind::Fragment
+    )
 );
