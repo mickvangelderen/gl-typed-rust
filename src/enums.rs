33e926c9354f20ed8e7e47a5e137a061d81bdd14
Mick van Gelderen <mickvangelderen@gmail.com>
2019-02-04 12:17:48 +0100

    Reduce duplication in enum defs

--- src/enums.rs
+++ src/enums.rs
@@ -3,98 +3,83 @@
 //! through the symbols.
 
 use crate::gl;
-use crate::symbols;
 
-macro_rules! impl_unknown_from {
-    ($T: path) => {
-        impl From<$T> for symbols::Unknown {
-            fn from(_: $T) -> Self {
-                symbols::Unknown
-            }
-        }
-    };
-}
-
-macro_rules! impl_variants {
-    ($e: ident { $($v: ident,)* }) => {
+macro_rules! impl_enums_u32 {
+    ($($(#[$em:meta])* pub enum $e:ident { $($v:ident = $g:path,)* })*) => {
         $(
-            impl_variants!(
-                enum = $e,
-                variant = $e::$v,
-                symbol = symbols::$v,
-            );
-        )*
-    };
-    (enum = $enum: path, variant = $variant: path, symbol = $symbol: path,) => {
-        impl From<$symbol> for $enum {
-            fn from(_: $symbol) -> Self {
-                $variant
+            $(#[$em])*
+            #[derive(Debug, Clone, Copy, Eq, PartialEq)]
+            #[repr(u32)]
+            pub enum $e {
+                $(
+                    $v = $g,
+                )*
             }
-        }
-    };
-}
 
-/// The kind of a shader.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-#[repr(u32)]
-pub enum ShaderKind {
-    Compute = gl::COMPUTE_SHADER,
-    Vertex = gl::VERTEX_SHADER,
-    TessControl = gl::TESS_CONTROL_SHADER,
-    TessEvaluation = gl::TESS_EVALUATION_SHADER,
-    Geometry = gl::GEOMETRY_SHADER,
-    Fragment = gl::FRAGMENT_SHADER,
-}
+            impl $e {
+                #[inline]
+                pub unsafe fn from_u32_unchecked(v: u32) -> Self {
+                    std::mem::transmute(v)
+                }
+            }
 
-impl_unknown_from!(ShaderKind);
+            impl From<u32> for $e {
+                #[inline]
+                fn from(v: u32) -> Self {
+                    match v {
+                        $(
+                            $g => $e::$v,
+                        )*
+                        v => panic!("$e has no variant corresponding to {}.", v),
+                    }
+                }
+            }
 
-impl_variants!(
-    ShaderKind {
-        Compute,
-        Vertex,
-        TessControl,
-        TessEvaluation,
-        Geometry,
-        Fragment,
+            $(
+                impl From<crate::symbols::$v> for $e {
+                    #[inline]
+                    fn from(_: crate::symbols::$v) -> Self {
+                        $e::$v
+                    }
+                }
+            )*
+
+            impl From<$e> for crate::symbols::Unknown {
+                #[inline]
+                fn from(_: $e) -> Self {
+                    crate::symbols::Unknown
+                }
+            }
+        )*
     }
-);
-
-/// The compile status of a shader.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-#[repr(u32)]
-pub enum CompileStatus {
-    Uncompiled = gl::FALSE as u32,
-    Compiled = gl::TRUE as u32,
 }
 
-impl_unknown_from!(CompileStatus);
+const TRUE: u32 = gl::TRUE as u32;
+const FALSE: u32 = gl::FALSE as u32;
 
-impl_variants!(
-    CompileStatus {
-        Uncompiled,
-        Compiled,
+impl_enums_u32! {
+    /// The kind of a shader.
+    pub enum ShaderKind {
+        Compute = gl::COMPUTE_SHADER,
+        Vertex = gl::VERTEX_SHADER,
+        TessControl = gl::TESS_CONTROL_SHADER,
+        TessEvaluation = gl::TESS_EVALUATION_SHADER,
+        Geometry = gl::GEOMETRY_SHADER,
+        Fragment = gl::FRAGMENT_SHADER,
     }
-);
 
-/// Allowed pname arguments to `glGetShaderiv`.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-#[repr(u32)]
-pub enum GetShaderivParam {
-    ShaderType = gl::SHADER_TYPE,
-    DeleteStatus = gl::DELETE_STATUS,
-    CompileStatus = gl::COMPILE_STATUS,
-    InfoLogLength = gl::INFO_LOG_LENGTH,
-    ShaderSourceLength = gl::SHADER_SOURCE_LENGTH,
+    /// The compile status of a shader.
+    pub enum CompileStatus {
+        Uncompiled = FALSE,
+        Compiled = TRUE,
+    }
+
+    /// Allowed pname arguments to `glGetShaderiv`.
+    pub enum GetShaderivParam {
+        ShaderType = gl::SHADER_TYPE,
+        DeleteStatus = gl::DELETE_STATUS,
+        CompileStatus = gl::COMPILE_STATUS,
+        InfoLogLength = gl::INFO_LOG_LENGTH,
+        ShaderSourceLength = gl::SHADER_SOURCE_LENGTH,
+    }
 }
-
-impl_unknown_from!(GetShaderivParam);
-
-impl_variants!(
-    GetShaderivParam {
-        ShaderType,
-        DeleteStatus,
-        CompileStatus,
-        InfoLogLength,
-        ShaderSourceLength,
-    }
-);
